8334a001f046663d1e3ad83e61fd1cd0aa440659
sam8beard <sammybeard2002@gmail.com>
2025-06-06 20:52:48 -0400

    adjusted dir structure

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sam8beard/csv-json-api
 
 go 1.24.3
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require github.com/go-chi/chi/v5 v5.2.1
---
+++ internal/handlers/upload.go
@@ -0,0 +1,13 @@
+package handlers 
+
+import ( 
+	"net/http"
+	// "fmt"
+	// "github.com/go-chi/chi/v5"
+	// "github.com/go-chi/chi/v5/middleware"
+)
+
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) { 
+
+} // uploadHandler 
