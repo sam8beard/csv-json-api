4a6ffa891c61e9ec699ba4dbd5210e4ad8b31ea1
sam8beard <sammybeard2002@gmail.com>
2025-06-07 03:28:02 -0400

    cleaned up error checking and formatted response

--- internal/handlers/upload.go
+++ internal/handlers/upload.go
@@ -21,11 +21,11 @@ type Response struct {
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) { 
 	response := Response{}
-	response.SkippedFiles := []string{}
-	response.ConvertedFiles := []string{} 
-	response.ZipURL := ""
-	response.FilesSkipped := 0
-	response.FilesProcessed := 0
+	response.SkippedFiles = []string{}
+	response.ConvertedFiles = []string{} 
+	response.ZipURL = ""
+	response.FilesSkipped = 0
+	response.FilesProcessed = 0
 
 	if r.Method != "POST" { 
 		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
@@ -38,19 +38,17 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unable to parse form", http.StatusBadRequest)
 	} // if 
 
-
 	// If file form fields are supplied, iterate through map and process
 	if len(r.MultipartForm.File) != 0 {
 		if r.MultipartForm.File["files"] != nil { 
 			for _, header := range r.MultipartForm.File["files"] {
 				file_extension := filepath.Ext(header.Filename)
-				if file_extension == ".csv" || file_extension == "json" {
+				if file_extension == ".csv" || file_extension == ".json" {
 					
 					// pass to validate then pass to convert? or
 					// call validate in convert? 
 
 				} else { 
-					
 					/* 
 					ERROR: 
 
@@ -60,9 +58,8 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 					response.SkippedCounter++
 					msg := "file " + header.Filename + " skipped: invalid file extension. Must be .csv or .json"
 					response.SkippedFiles = append(response.SkippedFiles, msg)
-					
-
-				}
+					continue 
+				} // if 
 				fmt.Fprintf(w, "Size of %s: %v bytes\n", header.Filename, header.Size)
 			} // for 
 		} else { 
@@ -73,29 +70,27 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	// If non-file form fields are supplied, iterate through map and process
 	if len(r.MultipartForm.Value) != 0 { 
 		if r.MultipartForm.Value["urls"] != nil { 
-			for _, url := range r.MultipartForm.Value["urls"] { 
-				parsedUrl, err := url.Parse(url)
+			for _, rawUrl := range r.MultipartForm.Value["urls"] { 
+				parsedUrl, err := url.Parse(rawUrl)
 				if err != nil { 
 					/* 
 					ERROR: Add something to error report, invalid url 
 					*/
 					response.SkippedCounter++
-					msg := "URL " + url + " skipped: could not parse"
+					msg := "URL " + rawUrl + " skipped: could not parse"
 					response.SkippedFiles := append(response.SkippedFiles, msg)
+					continue
 				} // if 
-				file_extension := filepath.Ext(parsedUrl)
+				file_extension := filepath.Ext(parsedUrl.Path)
 				if file_extension == ".csv" || file_extension == ".json" { 
 					/*
 					VALID: Download then convert
 					*/
 				} else { 
-					/* 
-					ERROR: Add to report, wrong type of file 
-					*/
 					response.SkippedCounter++
 					msg := "URL" + parsedUrl + " skipped: unsupported file type"
 					response.SkippedFiles := append(response.SkippedFiles, parsedUrl)
-
+					continue
 				} // if 
 				fmt.Fprintln(w, url)
 			} // for	
@@ -103,4 +98,13 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "For urls, please use the field name, 'urls'.")
 		} // if 
 	} // if 
+	
+	/*
+		Eventually, this will also be populated with the remaining response members
+	*/
+
+	encodedResponse, err := json.Marshal(response)
+	w.SetHeader("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(endcodedResponse)
 } // UploadHandler 
