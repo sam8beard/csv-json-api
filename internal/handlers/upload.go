06f2f9e4ec7de586e4c27ed499a3cdd986b9b207
sam8beard <sammybeard2002@gmail.com>
2025-06-06 21:08:09 -0400

    began working on upload handler

--- internal/handlers/upload.go
+++ internal/handlers/upload.go
@@ -2,6 +2,7 @@ package handlers
 
 import ( 
 	"net/http"
+	"github.com/sam8beard/csv-json-api/utils"
 	// "fmt"
 	// "github.com/go-chi/chi/v5"
 	// "github.com/go-chi/chi/v5/middleware"
@@ -9,5 +10,40 @@ import (
 
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) { 
+	if r.Method != "POST" { 
+		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
+	} // if 
 
-} // uploadHandler 
+	// populate Multipart Form to retrieve file and file header 
+	err := r.ParseMultipartForm(10 << 20)
+	
+	if err != nil { 
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+	} // if 
+
+	// If file form fields are supplied, iterate through map and process
+	if len(r.MultipartForm.File) != 0 {
+		if r.MultipartForm.File["files"] != nil { 
+			for _, header := range r.MultipartForm.File["files"] {
+				fileReader, err := header.Open(); if err != nil { fmt.Println(err)}
+				contents, err := io.ReadAll(fileReader); if err != nil { fmt.Println(err)}
+				_ = fileReader
+				_ = contents 
+				fmt.Fprintf(w, "Size of %s: %v bytes\n", header.Filename, header.Size)
+			} // for 
+		} else { 
+			fmt.Fprintln(w, "For files, please use the field name, 'files'.")
+		}
+	} // if 
+	
+	// If non-file form fields are supplied, iterate through map and process
+	if len(r.MultipartForm.Value) != 0 { 
+		if r.MultipartForm.Value["urls"] != nil { 
+			for _, value := range r.MultipartForm.Value["urls"] { 
+				fmt.Fprintln(w, value)
+			} // for	
+		} else { 
+			fmt.Fprintln(w, "For urls, please use the field name, 'urls'.")
+		} // if 
+	} // if 
+} // UploadHandler 
---
+++ internal/utils/download.go
@@ -0,0 +1,10 @@
+package utils
+
+import ( 
+	"fmt"
+	"os	"
+)
+
+func DownloadFile(string url) (*os.File) { 
+	
+} // DownloadFile
