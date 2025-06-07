8fde208338dd3500dca5a233105b9fe870389d56
sam8beard <sammybeard2002@gmail.com>
2025-06-06 22:09:36 -0400

    modified args and return types of convert funcs

---
+++ internal/utils/convert.go
@@ -0,0 +1,14 @@
+package utils 
+
+import ( 
+	"fmt"
+	"os"
+)
+
+func ConvertToJSON(r io.Reader) ([]byte, error) { 
+
+} // ConvertToJSON
+
+func ConvertToCSV(r io.Reader) ([]byte, error) { 
+
+} // ConvertToCSV
