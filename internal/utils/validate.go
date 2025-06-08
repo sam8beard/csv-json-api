c77eb65df4e939868d6286c31506336ed7d9f703
sam8beard <sammybeard2002@gmail.com>
2025-06-07 20:41:00 -0400

    added validate csv test and test files

--- internal/utils/validate.go
+++ internal/utils/validate.go
@@ -15,7 +15,8 @@ I know that encoding/json literally has a built in function to
 validate json objects and this is redundant in every way, 
 but I'm instantiating my own function for my sanity.
 
-I'm just wrapping the call to json.Validate in this function.
+I'm just wrapping the call to json.Validate in this function 
+and returning an error instead of bool.
 */
 func ValidateJSON(r io.Reader) error {
 	fileContents := r.ReadAll() 
