5556dc5814868085ef731eb9773eaef1ad53ae4f
sam8beard <sammybeard2002@gmail.com>
2025-06-07 21:26:35 -0400

    added more tests

--- internal/utils/test/validate_test.go
+++ internal/utils/test/validate_test.go
@@ -13,12 +13,25 @@ func TestValidateCSV(t *testing.T) {
 func TestValidateJSON(t *testing.T) { 
 	invalidFilePath := "../test-files/test2-invalid.json"
 	invalidReader, err := os.Open(invalidFilePath); if err != nil {fmt.Println("Cannot open file")}
-	validErr := ValidateJSON(invalidReader) 
+	invalidErr := ValidateJSON(invalidReader) 
+	if invalidErr != nil { 
+		t.Log("Test 1 passed")
+		
+	} else { 
+		t.Log("Test 1 failed")
+	} // if 
+	invalidReader.Close()
+
+	validFilePath := "../test-files/test2-valid.json"
+	validReader, err := os.Open(validFilePath); if err != nil {fmt.Println("Cannot open file")}
+	validErr := ValidateJSON(validReader)
 	if validErr != nil { 
-		t.Log("Test passed")
-		return
-	} 
-	t.Log("Test failed")
+		t.Log("Test 2 passed")
+		
+	} else { 
+		t.Log("Test 2 failed")
+	} // if 
+	validReader.Close()
 	return
 
 } // TestValidateJSON
