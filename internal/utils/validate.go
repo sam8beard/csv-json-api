faa7f4999433b577c8764af86dabfa448dbe3919
sam8beard <sammybeard2002@gmail.com>
2025-06-07 20:24:48 -0400

    validate json mostly completed

--- internal/utils/validate.go
+++ internal/utils/validate.go
@@ -2,11 +2,26 @@ package utils
 
 import ( 
 	"io"
+	"encoding/json"
+	"errors"
 )
 func ValidateCSV(r io.Reader) error { 
 	// Check if suffix .csv
+	
 } // ValidateCSV
 
+/*
+I know that encoding/json literally has a built in function to 
+validate json objects and this is redundant in every way, 
+but I'm instantiating my own function for my sanity.
+
+I'm just wrapping the call to json.Validate in this function.
+*/
 func ValidateJSON(r io.Reader) error {
-	// Check if suffix .json
+	fileContents := r.ReadAll() 
+	err := nil 
+	if json.Valid(fileContents) != true { 
+		error := errors.New("Invalid file format")
+	} // if 
+	return err
 } // ValidateJSON
