baf33691587f8ecac20d4c3afece815b136462e1
sam8beard <sammybeard2002@gmail.com>
2025-06-08 02:00:58 -0400

    began working on convert functions

---
+++ internal/utils/test/convert.go
@@ -0,0 +1,14 @@
+package test
+
+// import ( 
+// 	"fmt"
+// 	// "os"
+// )
+
+// func ConvertToJSON(r io.Reader) ([]byte, error) { 
+	
+// } // ConvertToJSON
+
+// func ConvertToCSV(r io.Reader) ([]byte, error) { 
+
+// } // ConvertToCSV
---
+++ internal/utils/test/convert_test.go
@@ -0,0 +1,10 @@
+package test
+
+import ( 
+	"fmt"
+	"testing"
+)
+
+func TestConvert(t *testing.T) { 
+	fmt.Println("Placeholder")
+} // TestSomething
