090aed314a23a01b78db90108f0c2ae1b97f4585
sam8beard <sammybeard2002@gmail.com>
2025-06-10 21:21:20 -0400

    added comments for reorganizing logic

--- internal/utils/test/download.go
+++ internal/utils/test/download.go
@@ -9,18 +9,36 @@ import (
 	"io"
 )
 
+/* 
+Handle url verification logic here
+
+Cases to account for: 
+
+1. Unparsable URLS 
+2. Get files using http request -> verify file by... 
+	1. Checking content type AND THEN 
+	2. Call Validate[File Type] function 
+
+*/ 
 func DownloadFile(url string) (io.ReadCloser, error) { 
 	var funcErr error 
 	response, err := http.Get(url)
+
+	// file not found at url given
 	if err != nil { 
 		 funcErr = errors.New("URL " + url + " skipped: file does not exist at specified location")
 		 return nil, funcErr
 	} // if 
 	
+	// parse URL and check error, return correct response 
+
+	// check content-type, if passes -> call validate
+	// if validate doesnt return error -> we are OK to convert 
+	
 	// Validate file formatting 
 	contentType := response.Header.Get("Content-Type")
 	fmt.Println(contentType)
-
+	
 	if contentType strings.Contains("application/json")
 
 	// if (filePath.Ext(url) == ".csv") { 
